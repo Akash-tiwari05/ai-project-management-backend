c3369fed7a61841f163eb4762f0bd890077b591f
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-06-21 19:29:37 +0530

    ai-project-management-setup

---
+++ src/test/java/com/akash/aipm/AiProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.aipm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
